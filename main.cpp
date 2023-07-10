28f105f5f6f0bf4eda8250eeec725aa083100eea
DogRacer78 <doggieracer@hotmail.com>
2023-07-10 13:55:40 +0000

    Tutorial Code Completed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "mbed.h"
 
+extern "C" int Mul2(int num);
+extern "C" void ReverseString(char word[], int size);
+
 // main() runs in its own thread in the OS
 int main()
 {
+    // define the number
+    int num = 1000;
+
+    // call the assembly
+    num = Mul2(num);
+    
+    // printf the result
+    printf("number: %d\r\n", num);
+
     while (true) {
 
     }
